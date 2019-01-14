eafd9e6d2af292e2f7bcef9c472986de60203d88
gloria <pureainu@gmail.com>
2019-01-14 18:34:13 +0900

    chore: off prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,35 +12,25 @@ module.exports = {
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     "plugin:vue/recommended",
-    "plugin:prettier/recommended",
+    // "plugin:prettier/recommended",
   ],
   // required to lint *.vue files
   plugins: [
-    "prettier",
+    // "prettier",
     "vue",
   ],
   // add your custom rules here
   rules: {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    "prettier/prettier": [
-      "error",
-      {
-        "singleQuote": true,
-        "trailingComma": "all",
-      },
-    ],
+    // "prettier/prettier": [
+    //   "error",
+    //   {
+    //     "singleQuote": true,
+    //     "trailingComma": "all",
+    //   },
+    // ],
     "semi": [2, "always"],
     "vue/max-attributes-per-line": "off",
-    "vue/component-name-in-template-casing": [
-      "error",
-      "PascalCase",
-      {
-        "ignores": [
-          "nuxt",
-          "nuxt-link"
-        ]
-      }
-    ],
   }
 };
