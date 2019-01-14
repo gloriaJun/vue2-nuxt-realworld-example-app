e14ba91cc9d709619d117bdfc9296a1db224388f
gloria <pureainu@gmail.com>
2019-01-14 16:13:15 +0900

    chore: build configuration for lint

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,13 @@ export default {
   head: {
     title: 'Conduit',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        charset: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
     ],
     link: [
       {
@@ -12,20 +17,36 @@ export default {
       },
       {
         rel: 'stylesheet',
-        href: '//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic',
+        href:
+          '//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic',
       },
       {
         rel: 'stylesheet',
         href: '//demo.productionready.io/main.css',
       },
-    ]
+    ],
   },
 
+  /**
+   * Build configuration
+   */
   build: {
-    vendor: [
-      'axios',
-    ],
+    vendor: ['axios'],
     analyze: true,
+
+    /*
+      You can extend webpack config here
+     */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+        });
+      }
+    },
   },
 };
-
