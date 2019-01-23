511c78797a7a44fc1ada55149ea6f7ef2e2371cf
gloria <pureainu@gmail.com>
2019-01-23 15:30:22 +0900

    feat: add banner component

---
+++ components/app-banner.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="banner">
+    <div class="container">
+      <h1 class="logo-font">
+        {{ siteName }}
+      </h1>
+      <p>A place to share your knowledge.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppBanner',
+  props: {
+    siteName: {
+      type: String,
+      default: '',
+      require: false,
+    },
+  },
+};
+</script>
